58656c36ad48b939951c620b2bf0a8100664aac6
carot750 <brehmersa@gmail.com>
2018-04-25 01:59:48 +0000

    made modifications to the script utilizing new knowledge of strings and several other items. Next on list to fix is that the file is overwritten when archived. will be attempting to either append the log or write iterations based on existing logs.

--- system_info/info_gather.py
+++ system_info/info_gather.py
@@ -6,32 +6,28 @@ import fnmatch
 import subprocess
 from pathlib import Path
 import datetime
+import glob
 
 home=os.environ['HOME']
 today=datetime.date.today()
 today=today.strftime('%y%m%d')
 ofile=os.path.join(home,'status.' + today + '.txt')
 archive=os.path.join(home,'archive/')
+wt="war"
 
 def wrifile(filename,value,wrtype,action):
-    if wrtype == 0:
-        with open(filename, "w") as outfile:
-            if action == '0':
-                act=writeln(value)
-            elif action == '1':
-                subprocess.call(cmd , stdout=outfile)
-    elif wrtype == 1:
-        with open(filename, "a") as outfile:
-            if action == '0':
-                act=writeln(value)
-            elif action == '1':
-                subprocess.call(cmd , stdout=outfile)
+    with open(filename, wt[wrtype]) as outfile:
+            if action == 0:
+                outfile.writelines(value)
+            elif action == 1:
+                subprocess.call(value , stdout=outfile)
+
 
 def get_files(path,value):
     matches = []
-    for root, dirs, files in os.walk(path):
-        for filename in fnmatch.filter(files,value):
-            matches.append(filename)
+    os.chdir(path)
+    for filename in glob.glob(value):
+        matches.append(filename)
     # Sort files alphabetically
     matches.sort()
 
@@ -42,54 +38,47 @@ def ensure_dir(path):
         os.makedirs(path)
 
 def mov_files(path,dfolder,value):
-
     # Create destination folder if it doesn't exist.
     ensure_dir(dfolder)
-
     matches = get_files(path,value)
     for i in range(len(matches)):
-        shutil.move(os.path.join(path,matches[i]),os.path.join(dfolder,matches[i]))
+            shutil.move(os.path.join(pals -lartth,matches[i]),os.path.join(dfolder,matches[i]))
+
 
 def logging(step):
     if step == '0':
         filechk=get_files(home,"status.*.txt")
         filechkc=len(filechk)
         if filechkc > 0:
+            print(filechk)
+            print(filechkc)
             darchpath=Path(archive+"/"+today)
             darchpath=str(darchpath)
             ensure_dir(darchpath)
-            mov_files(home,archive+"/"+today,"status.*.txt")
+        try:
+            mov_files(home,archive+today,"status.*.txt")
+        except:
+            print('home: '+home+' archive: '+archive+' filechk: '+str(filechk))
 
-        wrifile(ofile,"Begin Status",0,0)
-        wrifile(ofile," ",1,0)
-        wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
+        wrifile(ofile,"Begin Status\n\n",0,0)
+        wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n",1,0)
     elif step == '1':
-        wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
-        wrifile(ofile," ",1,0)
-        wrifile(ofile,"End Status",1,0)
+        wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n",1,0)
+        wrifile(ofile,"End Status\n",1,0)
     else:
-        wrifile(ofile,"Something went wrong!  Danger! Danger! Danger!",1,0)
+        wrifile(ofile,"Something went wrong!  Danger! Danger! Danger!\n\n",1,0)
 
 def drive():
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
-    wrifile(ofile,"Start of Phase 1: Storage Usage: ",1,0)
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"df -h",1,1)
-    wrifile(ofile,"End of Phase 1: Storage Usage: ",1,0)
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
-    wrifile(ofile," ",1,0)
+    wrifile(ofile,"Start of Phase 1: Storage Usage: \n\n",1,0)
+    wrifile(ofile,['df','-h'],1,1)
+    wrifile(ofile,"\nEnd of Phase 1: Storage Usage.\n\n",1,0)
+    wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n",1,0)
 
 def ino():
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
-    wrifile(ofile,"Start of Phase 2: Inode Usage: ",1,0)
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"df -i",1,1)
-    wrifile(ofile," ",1,0)
-    wrifile(ofile,"End of Phase 2: Inode Usage: ",1,0)
-    wrifile(ofile," ",1,0)
+    wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n",1,0)
+    wrifile(ofile,"Start of Phase 2: Inode Usage: \n\n",1,0)
+    wrifile(ofile,['df','-i'],1,1)
+    wrifile(ofile,"\nEnd of Phase 2: Inode Usage.\n\n",1,0)
     wrifile(ofile,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",1,0)
 
 def mainprocess():
